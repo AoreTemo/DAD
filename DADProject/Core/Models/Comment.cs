5bf5e72b4e04d2b5a852cd2af1932a21f1f0d2d1
a.ot_m <denshpk@gmail.com>
2023-08-24 01:00:32 +0300

    Added nullable types

--- DADProject/Core/Models/Comment.cs
+++ DADProject/Core/Models/Comment.cs
@@ -2,8 +2,8 @@ namespace Core.Models;
 
 public class Comment : Content
 {
-    public int PostId { get; set; }
-    public Post Post { get; set; }
+    public int? PostId { get; set; }
+    public Post? Post { get; set; }
     public List<Comment> Replies { get; set; } = new List<Comment>(); // Replies on this comment
     public int? RepliedCommentId { get; set; }
     public Comment? RepliedComment { get; set; } // Comment that we are replying
