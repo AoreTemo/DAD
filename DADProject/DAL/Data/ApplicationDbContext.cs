a9f95f51a28814f34c79ea4321496e3de4c0f29d
Artem <temaprgod8@gmail.com>
2023-08-24 12:16:55 +0300

    Creating AppDbContext

---
+++ DADProject/DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Models;
+
+namespace DAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        #region DbSets
+        DbSet<AppUser> Users { get; set; } = null!;
+        DbSet <Comment> Comments { get; set; } = null!;
+        DbSet<Post> Posts { get; set; } = null!;
+        DbSet<Notification> Notifications { get; set; } = null!;
+        DbSet<Message> Messages { get; set; } = null!;
+        DbSet<Media> Medias { get; set; } = null!;
+
+        #endregion
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
