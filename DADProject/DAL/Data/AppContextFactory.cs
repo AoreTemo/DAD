145044ec592b8d933907ba3cf88051fd6aa5dbdd
Artem <temaprgod8@gmail.com>
2023-08-24 13:01:21 +0300

    Add AppContextFactory

---
+++ DADProject/DAL/Data/AppContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+
+namespace DAL.Data
+{
+    internal class AppContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        private DbContextOptionsBuilder<ApplicationDbContext> GetDbContextOptionBuilder()
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+
+        }
+
+    }
+}
