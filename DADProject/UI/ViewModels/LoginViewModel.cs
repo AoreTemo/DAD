92216112c149c6681989618d3167839311035e24
Artem <temaprgod8@gmail.com>
2023-08-27 18:58:33 +0300

    Added LoginViewModel

---
+++ DADProject/UI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
+
+namespace UI.ViewModels;
+
+    public class LoginViewModel
+    {
+        [Display(Name = "EmailAddress")]
+        [Required(ErrorMessage = "Email adress field is required for filling")]    
+        public string EmailAddress { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+
