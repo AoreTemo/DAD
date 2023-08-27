8d3860cda70f51875ada949ca66160382bc75ad8
Artem <temaprgod8@gmail.com>
2023-08-27 18:56:51 +0300

    Add Login page

--- DADProject/UI/Program.cs
+++ DADProject/UI/Program.cs
@@ -3,6 +3,7 @@ using DAL.Abstractions.Repository;
 using DAL.Data;
 using DAL.Repository;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -15,7 +16,11 @@ Log.Logger = new LoggerConfiguration()
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+{ 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddScoped<UserManager<AppUser>>();
 
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddDefaultTokenProviders().AddRoles<IdentityRole>()
