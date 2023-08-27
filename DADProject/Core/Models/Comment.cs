866316cc6e0d470b81d37bfbfad1e900abb08e0c
a.ot_m <denshpk@gmail.com>
2023-08-27 16:02:49 +0300

    Removed useless pieces of code

--- DADProject/Core/Models/Comment.cs
+++ DADProject/Core/Models/Comment.cs
@@ -4,7 +4,7 @@ public class Comment : Content
 {
     public int? PostId { get; set; }
     public Post? Post { get; set; }
-    public List<Comment> Replies { get; set; } = new List<Comment>(); // Replies on this comment
     public int? RepliedCommentId { get; set; }
-    public Comment? RepliedComment { get; set; } // Comment that we are replying
+    public Comment? RepliedComment { get; set; }
+    public List<Comment> Replies { get; set; } = new List<Comment>();
 }
--- DADProject/Core/Models/Notification.cs
+++ DADProject/Core/Models/Notification.cs
@@ -4,9 +4,9 @@ namespace Core.Models;
 
 public class Notification : BaseEntity
 {
-    public string Body { get; set; } = string.Empty;
+    public string Body { get; set; }
     public DateTime SendTime { get; set; }
     public NotificationState NotificationState { get; set; }
-    public string? AppUserId { get; set; } = string.Empty;
+    public string? AppUserId { get; set; }
     public AppUser? AppUser { get; set; }
 }
